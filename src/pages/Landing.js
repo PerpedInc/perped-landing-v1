398df2a14364ef25978b96bec802b28e36a1328f
Vansh Wassan <007blackhacker@gmail.com>
2022-11-18 02:41:23 +0530

    Title and favicon: @vanshwassan

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Logo from '../components/ui/awd.gif'
 import Hero from "../components/sections/Hero";
 import LandingLayout from "../components/layouts/LandingLayout";
 
+
 export default function Landing() {
+  useEffect(() => {
+    document.title = 'Perped - A Decentralised Perpetuals Protocol';
+  }, []);
   return (
     <LandingLayout>
       <Hero
