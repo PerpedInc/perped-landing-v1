3b11f3867acd79dc3fc2efda26d1141f34a6fdd8
br0wnD3v <bothereum@protonmail.com>
2023-06-11 21:54:54 +0530

    Font loading.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
+import "./fonts.css";
+
+import { useEffect, useState } from "react";
 
 import LandingLayout from "./components/layouts/LandingLayout";
 
 function App() {
-  return (
-    <>
-      <LandingLayout />
-    </>
-  );
+  const [isFontLoaded, setIsFontLoaded] = useState(false);
+
+  useEffect(() => {
+    // Check if the font is loaded
+    document.fonts.ready.then(() => {
+      setIsFontLoaded(true);
+    });
+  }, []);
+
+  return <>{isFontLoaded ? <LandingLayout /> : null}</>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./fonts.css";
 import "./index.css";
 
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
